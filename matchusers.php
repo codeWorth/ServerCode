ce540cdbe36335bf9165521d3951f57dc29accc9
Drewpew <agcummings11@gmail.com>
2016-12-07 17:52:39 -0800

    Update matchusers.php

--- matchusers.php
+++ matchusers.php
@@ -2,7 +2,7 @@
 
 echo "Making match... <br/>";
 
-$db = mysqli_connect("10.0.1.121", "Drewpew", "Smucker77", "mm_queue");
+$db = mysqli_connect("10.0.1.121", "Drewpew", "", "mm_queue");
 
 mysqli_query($db, "INSERT INTO `matches` (`Player1_ID`, `Player2_ID`, `Player1_Data`, `Player2_Data`) VALUES ('', '', '', '')");
 
