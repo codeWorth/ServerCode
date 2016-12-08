2d199ce179487f7b0a0df17ad76931ea8aa55db0
Drewpew <agcummings11@gmail.com>
2016-12-07 17:53:06 -0800

    Update area.php

--- area.php
+++ area.php
@@ -3,7 +3,7 @@
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-if ($username == "Drewpew" and $password == "Smucker77"){
+if ($username == "Drewpew" and $password == ""){
 	echo "YOUR INTO THE SYSTEM DODODODOODOD";
 } else {
 	echo "lol nope";
